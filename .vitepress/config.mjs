40f29dad906ef3cd28318f4ffd591aad02d323f7
wuwu <1945334772@qq.com>
2024-03-09 17:03:49 +0800

    first commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "学习资料库",
+  description: "A VitePress Site",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '首页', items:[
+        {text: '首页',link: '/'},
+        {text: '前端',link: '/markdown-examples'}
+      ] },
+      { text: '示例', link: '/markdown-examples' },
+      { text: '示例', link: '/markdown-examples' },
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/dawnlight129' }
+    ],
+    footer: { 
+      copyright:"Copyright @ 2024-present wuwu"
+    }
+    
+  }
+})
---
+++ index.md
@@ -0,0 +1,27 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "学习资料库"
+  text: ""
+  tagline: 慢慢来，谁还没个努力的过程
+  image:
+    src: ''
+  actions:
+    - theme: brand
+      text: Markdown 示例
+      link: /markdown-examples
+    - theme: alt
+      text:   前 端 面 经  
+      link: https://interview.html5.wiki/
+
+features:
+  - title: Vue
+    details: Vue.js 是一款流行的前端 JavaScript 框架，它可以帮助开发者快速构建交互性强、响应速度快的用户界面
+  - title: React
+    details: React是一个javascript库，不是一个架构，是由Facebook创建的一个开源项目
+  - title: Node.js
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
